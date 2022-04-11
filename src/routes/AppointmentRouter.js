dde9559f0542c11b0c22d5e1d7e6681e0244a325
Diogo Pimentel <diogopimenteel@gmail.com>
2022-04-11 14:21:53 -0300

    create a middleware validation on routes

--- src/routes/AppointmentRouter.js
+++ src/routes/AppointmentRouter.js
@@ -1,15 +1,17 @@
 import express from 'express';
 
 import AppointmentController from '../controllers/AppointmentController.js';
+import AppointmentValidation from '../validations/AppointmentValidation.js';
+import validation from '../validations/ValidationMiddleware.js';
 
 const router = express.Router();
 const appointmentController = new AppointmentController();
 
 router.get('/api/appointment', appointmentController.index);
 
-router.post('/api/appointment', appointmentController.store);
+router.post('/api/appointment', validation(AppointmentValidation), appointmentController.store);
 
-router.put('/api/appointment/:id', appointmentController.update);
+router.put('/api/appointment/:id', validation(AppointmentValidation), appointmentController.update);
 
 router.delete('/api/appointment/:id', appointmentController.remove);
 
---
+++ src/validations/AppointmentValidation.js
@@ -0,0 +1,17 @@
+import * as yup from 'yup';
+
+const AppointmentValidation = yup.object().shape({
+  name: yup
+    .string()
+    .required('Name field required')
+    .min(3)
+    .matches(/^[A-Za-zà-úÀ-Ú ]+$/, 'Name must be only characters'),
+  birthday: yup.date()
+    .max(new Date(), 'Invalid birthday')
+    .required('Birthday field is required'),
+  selectedDate: yup.date()
+    .min(new Date(), 'Invalid selected date')
+    .required('Selected date is required'),
+});
+
+export default AppointmentValidation;
