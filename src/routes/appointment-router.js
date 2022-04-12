6030f796f6b655614b14913ee767a745c48dfa70
Diogo Pimentel <diogopimenteel@gmail.com>
2022-04-12 20:10:38 -0300

    created middleware folder and organize validations

--- src/routes/appointment-router.js
+++ src/routes/appointment-router.js
@@ -2,8 +2,8 @@ import express from 'express';
 
 import AppointmentController from '../controllers/appointment-controller.js';
 import inputValidation from '../validations/input-validation.js';
-import validateBusinessLogic from '../validations/rules-validation.js';
-import validation from '../validations/validation-middleware.js';
+import validateBusinessLogic from '../middleware/date-validation-middleware.js';
+import validation from '../middleware/common-validation-middleware.js';
 
 const router = express.Router();
 const appointmentController = new AppointmentController();
